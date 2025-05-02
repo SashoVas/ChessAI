2a7c2c1db033ac6a8a468fcb16882f18fa8a522f
mir4oni3 <m.stoyanovski3@gmail.com>
2025-05-02 21:48:11 +0300

    Added return status handling

--- ChessAI/src/main/java/com/ChessAI/exceptions/UserControllerException/UserAlreadyExistsException.java
+++ ChessAI/src/main/java/com/ChessAI/exceptions/UserControllerException/UserAlreadyExistsException.java
@@ -1,4 +1,4 @@
-package com.ChessAI.exceptions;
+package com.ChessAI.exceptions.UserControllerException;
 
 import org.springframework.web.bind.annotation.ResponseStatus;
 
---
+++ ChessAI/src/main/java/com/ChessAI/exceptions/UserControllerException/UserNotFoundException.java
@@ -0,0 +1,21 @@
+package com.ChessAI.exceptions.UserControllerException;
+
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = org.springframework.http.HttpStatus.NOT_FOUND)
+public class UserNotFoundException extends RuntimeException {
+    private static final String message = "User not found";
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+    public UserNotFoundException() {
+        super(message);
+    }
+    public UserNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public UserNotFoundException(Throwable cause) {
+        super(cause);
+    }
+}
