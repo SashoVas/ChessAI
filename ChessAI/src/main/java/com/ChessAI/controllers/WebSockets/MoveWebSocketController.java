1d9584e6a0dd1439dfe30cc093f72da19ccdddd5
SashoVas <98760930+SashoVas@users.noreply.github.com>
2025-05-05 19:28:44 +0300

    Adding authentication in web sockets

--- ChessAI/src/main/java/com/ChessAI/controllers/WebSockets/MoveWebSocketController.java
+++ ChessAI/src/main/java/com/ChessAI/controllers/WebSockets/MoveWebSocketController.java
@@ -3,12 +3,20 @@ package com.ChessAI.controllers.WebSockets;
 import com.ChessAI.dto.InitialConnectDTO;
 import com.ChessAI.dto.MoveInputDTO;
 import com.ChessAI.dto.MoveResultDTO;
+import com.ChessAI.models.UserPrincipal;
 import com.ChessAI.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+
+import java.security.Principal;
 
 @Controller
 
@@ -19,13 +27,18 @@ public class MoveWebSocketController {
     private GameService gameService;
 
     @MessageMapping("/game.initialConnect")
-    public void initialConnect(@Payload InitialConnectDTO input){
+    public void initialConnect(Principal principal, @Payload InitialConnectDTO input){
+        String username=principal.getName();
+        System.out.println(username);
+
+        //System.out.println(userDetails.getUsername());
         MoveResultDTO result=gameService.getCurrentGameState(input);
         String destination = "/room/game." + input.getRoomId();
         messagingTemplate.convertAndSend(destination, result);
     }
     @MessageMapping("/game.makeMoveToBot")
     public void moveOnBot(@Payload MoveInputDTO move) {
+        //System.out.println(userDetails.getUsername());
         System.out.println(move.move);
         System.out.println(move.roomId);
 
